dba7140b25e9001471b42d1b1f873da142a051d5
Winner Martins <Winner Martins@DESKTOP-G026B6C>
2018-05-21 21:08:56 -0300

    Tutorial Microsoft

---
+++ ActorClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using HelloWorld.Interfaces;
+
+namespace ActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHelloWorld actor = ActorProxy.Create<IHelloWorld>(ActorId.CreateRandom(), new Uri("fabric:/MyApplication/HelloWorldActorService"));
+            Task<string> retval = actor.GetHelloWorldAsync();
+            Console.Write(retval.Result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Actors.Client;
+using HelloWorld.Interfaces;
+
+namespace HelloWorld
+{
+    [StatePersistence(StatePersistence.Persisted)]
+    internal class HelloWorld : Actor, IHelloWorld
+    {
+        public HelloWorld(ActorService actorService, ActorId actorId)
+            : base(actorService, actorId)
+        {
+        }
+
+        public Task<string> GetHelloWorldAsync()
+        {
+            return Task.FromResult("Hello from my reliable actor!");
+        }
+    }
+}
